86731f8efcacc067137daffe9f04b8b61bf84214
Hyun <52783941+HyunLee103@users.noreply.github.com>
2021-03-09 17:18:20 +0900

    미디 데이터 chunking을 위한 chunk_midi 구현 (#13)
    * fea) chunk_midi argument 추가
    
    * fea) chunk_midi 함수 구현
    
    * trucate 함수 캡슐화
    
    * sty) type hint 추가(chunk_midi, _truncate)
    
    * sty) arguments 오타 수정
    
    * sty) argument 명시를 위한 main 함수 추가
    
    * sty) main 함수 script로 위치 조정

---
+++ dioai/preprocessor/chunk_midi.py
@@ -0,0 +1,179 @@
+import os
+from glob import glob
+from os.path import join
+
+import numpy as np
+import pretty_midi
+
+
+def chunk_midi(
+    steps_per_sec, longest_allowed_space, minimum_chunk_length, midi_dataset_path, chunked_midi_path
+) -> None:
+    def _truncate(f: float, n: int, dtype="str") -> float:
+        """Truncates/pads a float f to n decimal places without rounding"""
+        s = "{}".format(f)
+        if "e" in s or "E" in s:
+            truncated_str = "{0:.{1}f}".format(f, n)
+            if dtype == "float":
+                return float(truncated_str)
+            else:
+                return truncated_str
+        i, _, d = s.partition(".")
+        truncated_str = ".".join([i, (d + "0" * n)[:n]])
+        if dtype == "float":
+            return float(truncated_str)
+        else:
+            return truncated_str
+
+    TRUNCATE_UNDER_NTH_DECIMAL = len(str(steps_per_sec)) - 1  # 소수점 아래 몇 자리 이후를 버릴지
+    STEP_IN_SEC = _truncate(1.0 / steps_per_sec, TRUNCATE_UNDER_NTH_DECIMAL, dtype="float")
+    # Remove drum, FX, woodblock, gunshot, ... among midi instruments
+    Instrument_Not_For_Melody = [
+        55,
+        96,
+        97,
+        98,
+        99,
+        100,
+        101,
+        102,
+        103,
+        113,
+        114,
+        115,
+        116,
+        117,
+        118,
+        119,
+        120,
+        121,
+        122,
+        123,
+        124,
+        125,
+        126,
+        127,
+    ]
+    midifiles = []
+    for x in os.walk(midi_dataset_path):
+        for y in glob(join(x[0], "*.mid")):
+            midifiles.append(y)
+        for y in glob(join(x[0], "*.MID")):
+            midifiles.append(y)
+        for y in glob(join(x[0], "*.midi")):
+            midifiles.append(y)
+        for y in glob(join(x[0], "*.MIDI")):
+            midifiles.append(y)
+
+    for file_idx, filename in enumerate(midifiles):
+        print(f"{file_idx}th file, filename: {filename}")
+        try:
+            midi_data = pretty_midi.PrettyMIDI(filename)
+        except OSError:
+            print("pretty_midi로 읽을 수 있는 형식의 mid 데이터가 아닙니다.")
+            continue
+
+        # First, remove instrument track not necessary for generating melody
+        instrument_idx_to_remove = []
+        for inst_idx, instrument in enumerate(midi_data.instruments):
+            # if instrument is drum or not suitable for melody
+            if instrument.is_drum or instrument.program in Instrument_Not_For_Melody:
+                instrument_idx_to_remove.append(inst_idx)
+            # if instrument has no notes in it, remove too.
+            if not instrument.notes:
+                instrument_idx_to_remove.append(inst_idx)
+
+        midi_data.instruments = [
+            instrument
+            for inst_idx, instrument in enumerate(midi_data.instruments)
+            if inst_idx not in instrument_idx_to_remove
+        ]
+        # if no instrument left after removing not necessary instruments, process next midi file
+        if not midi_data.instruments:
+            continue
+
+        for inst_idx, instrument in enumerate(midi_data.instruments):
+            new_notes_per_instrument = []
+            """
+            각 chunk간의 time space 를 측정하려면, 여러 방법이 있겠지만,
+            나는 전체 곡의 길이를 0.1초 단위로 나눈다음 (각 0.1초 구간을 bin이라고 한다)
+            note들을 순회하면서 각 note가 걸치는 bin들을 True로 바꾼다(채운다).
+            모든 note들에 대해 bin을 채우는 일을 다 하면,
+            note 사이의 시간 간격이 LONGEST_ALLOWED_SPACE 이상이면 별개의 chunk로 취급하며,
+            해당 chunk의 길이가 MINIMUM_CHUNK_LENGTH 이상일 때만 최종적으로 chunk로 인정하여,
+            이렇게 최종적으로 얻어진 각 chunk에 해당하는 note들을 추려 하나의 midi파일로 저장한다.
+            저장되는 midi파일의 이름은 "원본미디파일이름_{instrument_index}_{chunk_index_in_the_instrument} 이다"
+            a bin in time_bins will be filled(turn to True) if any note occupies the bin
+            time_bins is quantized into 1/STEPS_PER_SEC sec
+            """
+            time_bins_sec = np.arange(
+                0, instrument.get_end_time() + STEP_IN_SEC, STEP_IN_SEC
+            ).tolist()  # unit is 1/STEPS_PER_SEC
+            time_bins_sec = [_truncate(sec, TRUNCATE_UNDER_NTH_DECIMAL) for sec in time_bins_sec]
+            time_bins = [False] * len(time_bins_sec)
+            # time_bins = {str(floor_to_1st_place_decimal): False for time_bin in time_bins}
+            for note in instrument.notes:
+                note_start = _truncate(note.start, TRUNCATE_UNDER_NTH_DECIMAL)
+                note_end = _truncate(note.end, TRUNCATE_UNDER_NTH_DECIMAL)
+                note_start_sec_index = time_bins_sec.index(note_start)
+                note_end_sec_index = time_bins_sec.index(note_end)
+                for sec_index in range(note_start_sec_index, note_end_sec_index + 1):
+                    time_bins[sec_index] = True
+
+            chunk_start_idx = -1
+            chunk_end_idx = -1
+            note_idx_to_start_checking = 0
+            counter = 0
+            for time_idx, time_bin in enumerate(time_bins):
+                if time_bin:
+                    if (
+                        counter >= steps_per_sec * longest_allowed_space or chunk_start_idx == -1
+                    ):  # when new chunk bigins or when the first chunk from instrument begins
+                        chunk_start_idx = time_idx
+                    chunk_end_idx = time_idx
+                    counter = 0
+                else:
+                    counter += 1
+
+                if (
+                    counter == steps_per_sec * longest_allowed_space
+                    or time_idx == len(time_bins) - 1
+                ):
+                    if chunk_end_idx - chunk_start_idx >= steps_per_sec * minimum_chunk_length:
+
+                        chunk_start_sec = float(time_bins_sec[chunk_start_idx])
+                        chunk_end_sec = float(time_bins_sec[chunk_end_idx])
+
+                        note_chunk_start_idx = -1
+                        for note_idx in range(note_idx_to_start_checking, len(instrument.notes)):
+                            if (
+                                note_chunk_start_idx == -1
+                                and instrument.notes[note_idx].start >= chunk_start_sec
+                            ):
+                                note_chunk_start_idx = note_idx
+                                start_note_sec = instrument.notes[note_chunk_start_idx].start
+                            if instrument.notes[note_idx].start > chunk_end_sec + STEP_IN_SEC or (
+                                note_idx == len(instrument.notes) - 1
+                            ):  # or 뒤의 조건을 빠뜨리면 instrument의 끝과 맞닿아 있는 chunk가 포함이 안됨.
+                                # pull timelilnes of notes to start of first note
+                                for track_note in instrument.notes[note_chunk_start_idx:note_idx]:
+                                    track_note.start -= start_note_sec
+                                    track_note.end -= start_note_sec
+                                new_notes_per_instrument.append(
+                                    instrument.notes[note_chunk_start_idx:note_idx]
+                                )
+                                note_idx_to_start_checking = note_idx
+                                break
+
+            for i, notes in enumerate(new_notes_per_instrument):
+                new_midi_object = pretty_midi.PrettyMIDI()
+                new_instrument = pretty_midi.Instrument(program=instrument.program)
+                new_instrument.notes = notes
+                new_midi_object.instruments.append(new_instrument)
+                filename_without_extension = os.path.splitext(filename.split("/")[-1])[0]
+                new_midi_object.write(
+                    os.path.join(
+                        chunked_midi_path,
+                        filename_without_extension + f"_{inst_idx}_{instrument.program}_{i}.mid",
+                    )
+                )
--- preprocess.py
+++ preprocess.py
@@ -2,11 +2,65 @@
 
 import argparse
 
+from dioai.preprocessor.chunk_midi import chunk_midi
+
 
 def get_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser("미디 전처리 후 tfrecord 형식으로 변환합니다.")
+    parser = argparse.ArgumentParser("미디 전처리 후 Numpy array 형식으로 변환합니다.")
+    parser.add_argument(
+        "--steps_per_sec",
+        type=int,
+        default=10,
+        choices=[10, 100],
+        help="각 Chunk의 길이를 계산할 때 1./steps_per_sec 단위로 계산.",
+    )
+    parser.add_argument(
+        "--longest_allowed_space",
+        type=int,
+        default=2,
+        help="하나의 chunk안에서 제일 긴 space는 longest_allowed_space초를 넘을 수 없다",
+    )
+    parser.add_argument(
+        "--minimum_chunk_length",
+        type=int,
+        default=8,
+        help="길이가 minimum_chunk_length초 미만인 chunk는 누락된다",
+    )
+    parser.add_argument(
+        "--source_midi_dir",
+        type=str,
+        required=True,
+        help="전처리하고자 하는 midi파일들이 들어있는 폴더. 폴더 속 폴더 존재 가능",
+    )
+    parser.add_argument(
+        "--chunk_midi_dir",
+        type=str,
+        required=True,
+        help="전처리 후 만들어진 각 chunk 는 하나의 midi파일로 저장되는데, 이러한 midi 파일들이 저장되는 폴더",
+    )
     return parser
+
+
+def main(args):
+    # how to devide one second. should be 10 or 100 or 1000
+    STEPS_PER_SEC = args.steps_per_sec
+    # longest allowed space in a chunk, in second
+    LONGEST_ALLOWED_SPACE = args.longest_allowed_space
+    # minimum possible midi chunk, in second
+    MINIMUM_CHUNK_LENGTH = args.minimum_chunk_length
+
+    midi_dataset_path = args.source_midi_dir
+    chunked_midi_path = args.chunk_midi_dir
+
+    return chunk_midi(
+        steps_per_sec=STEPS_PER_SEC,
+        longest_allowed_space=LONGEST_ALLOWED_SPACE,
+        minimum_chunk_length=MINIMUM_CHUNK_LENGTH,
+        midi_dataset_path=midi_dataset_path,
+        chunked_midi_path=chunked_midi_path,
+    )
 
 
 if __name__ == "__main__":
     args, _ = get_parser().parse_known_args()  # noqa: F403
+    main(args)
