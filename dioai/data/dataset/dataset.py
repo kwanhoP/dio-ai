d26cd43a55778cf0e24c3c8c60502302008531ed
Hyun <52783941+HyunLee103@users.noreply.github.com>
2021-05-20 10:17:29 +0900

    fea) n_embed 필드가 있는 모델 config로 수정 (#144)

--- dioai/data/dataset/dataset.py
+++ dioai/data/dataset/dataset.py
@@ -71,7 +71,7 @@ class GPT2ChordMetaToNoteDataset(BaseDataset):
             split=split,
             chord_embedding_path=self.config.chord_embedding_path,
             num_meta=self.config.num_meta,
-            n_embed=self.config.n_embd,
+            n_embed=self.config.model.n_embd,
         )
 
     def prepare_dataset(self) -> Iterator:
